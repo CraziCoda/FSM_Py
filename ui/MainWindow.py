b6058253cccf84b9c66b38ef34c7c85a64efdcf4
crazicoda <crazicoda@gmail.com>
2025-05-03 17:20:12 +0000

    Add main layout setup to MainWindow class in PyQt6 application

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -1,8 +1,10 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QPushButton
 from PyQt6.QtCore import QSize, Qt
 from ui.menu_bar.menu import Menu
 
 class MainWindow(QMainWindow):
+    main_layout: QVBoxLayout | None  = None
+
     def __init__(self):
         super().__init__()
 
@@ -12,3 +14,10 @@ class MainWindow(QMainWindow):
 
         # menu bar
         Menu(self.menuBar(), self)
+
+        # main layout
+        widget = QWidget()
+        self.main_layout = QVBoxLayout()
+        
+        widget.setLayout(self.main_layout)
+        self.setCentralWidget(widget)
