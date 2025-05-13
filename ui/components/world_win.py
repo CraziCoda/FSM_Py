8f8059d961a8e5913627f53e78d2bb21563b12ef
crazicoda <crazicoda@gmail.com>
2025-05-13 22:42:24 +0000

    Add QListWidget for world selection and apply custom styling

--- ui/components/world_win.py
+++ ui/components/world_win.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QFrame, QLabel, QScrollArea, QPushButton
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QFrame, QLabel, QScrollArea, QPushButton, QListWidget
+from PyQt5.QtGui import QIcon, QColor
 from PyQt5.QtCore import Qt
+from ui.styles.components import world_list_style
 
 class WorldWin(QWidget):
     def __init__(self):
@@ -41,35 +42,21 @@ class WorldWin(QWidget):
         world_name = QLabel("Just a world")
         world_name.setStyleSheet("font-weight: 500; font-family: 'Segoe UI'; font-size: 14px; border: 1px solid #aaaacc; border-bottom: 1px groove #dddddd; background-color: #ffffff; padding: 5px;")
 
-        worlds_frame = QFrame()
-        worlds_frame_layout = QVBoxLayout()
-        worlds_frame_layout.setContentsMargins(0, 0, 0, 0)
-        worlds_frame_layout.setSpacing(0)
-        worlds_frame.setLayout(worlds_frame_layout)
+        world_list = QListWidget()
+        world_list_layout = QVBoxLayout()
+        world_list_layout.setContentsMargins(0, 0, 0, 0)
+        world_list_layout.setSpacing(0)
+        world_list.setLayout(world_list_layout)
 
         worlds_sample = ["Microwave", "Traffic Light", "Washing Machine", "Light Switch", "Door"]
-        i = 0
+        world_list.addItems(worlds_sample)
 
-        for world in worlds_sample:
-            world_label = QLabel(world)
-            style = "font-weight: 400; font-family: 'Segoe UI'; font-size: 14px; border: none; border-bottom: 1px groove #dddddd; padding: 5px 5px;"
-            if i % 2 == 0:
-                style += "background-color: #eeeeec;"
-            else:
-                style += "background-color: #ffffff;"
-            
-            world_label.setStyleSheet(style)
+        world_list.setStyleSheet(world_list_style)
 
-            world_label.setMaximumHeight(30)
-            world_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
-            worlds_frame_layout.addWidget(world_label, 0)            
-            i += 1
-
-        worlds_frame.setStyleSheet("background-color: #ffffff; border: 1px solid #aaaacc; border-top: none;")
-        worlds_frame_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        world_list_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         scroll = QScrollArea()
-        scroll.setWidget(worlds_frame)
+        scroll.setWidget(world_list)
         scroll.setWidgetResizable(True)
         scroll.setStyleSheet("background-color: #ffffff;")
 
---
+++ ui/styles/components.py
@@ -0,0 +1,20 @@
+world_list_style = """
+QListWidget {
+    font-family: 'Segoe UI';
+    color: #000000;
+}
+
+QListWidget::item {
+    border-bottom: 1px solid #eeeeee;
+    color: #000000;
+    font-family: 'Segoe UI';
+    font-size: 14px;
+    padding: 5px 0;
+    height: 20px;
+    margin: 0;c
+}
+
+QListWidget::item:selected {
+    margin: 0;
+}
+"""
