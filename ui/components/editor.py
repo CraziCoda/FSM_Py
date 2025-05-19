c1bb6d9942e0145fc82b18de6c1e4b18e203b4bb
crazicoda <crazicoda@gmail.com>
2025-05-19 13:47:14 +0000

    Add axis lines to Editor component; enhance scene setup with coordinate axes

--- ui/components/editor.py
+++ ui/components/editor.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
-from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsLineItem
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtCore import Qt
 
 
 class Editor(QGraphicsView):
@@ -12,3 +13,21 @@ class Editor(QGraphicsView):
         self.setRenderHint(QPainter.RenderHint.Antialiasing)
         self.setStyleSheet("background-color: white;")
 
+        axis_pen = QPen(Qt.GlobalColor.black, 1)
+
+        self.setSceneRect(-self.width() // 2, -self.height() // 2, self.width(), self.height())
+
+        y_axis = QGraphicsLineItem(0, -self.height() // 2, 0, self.height() // 2)
+        y_axis.setPen(axis_pen)
+
+        x_axis = QGraphicsLineItem(-self.width() // 2, 0, self.width() // 2, 0)
+        x_axis.setPen(axis_pen)
+
+        self.scene.addItem(y_axis)
+        self.scene.addItem(x_axis)
+
+
+    def mousePressEvent(self, event):
+        pos = self.mapToScene(event.pos())
+
+        print(pos)
