f0611a2d7590b6737dd9c0c98309b796a8111c95
crazicoda <crazicoda@gmail.com>
2025-05-21 22:51:26 +0000

    Refactor ToolBar layout and icon handling; adjust spacing and styling for improved UI consistency

--- ui/components/editor.py
+++ ui/components/editor.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsLineItem, QWidget, QVBoxLayout, QPushButton, QListWidget
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsLineItem, QWidget, QVBoxLayout, QPushButton, QListWidget, QListWidgetItem
 from PyQt5.QtGui import QPainter, QPen, QIcon
 from PyQt5.QtCore import Qt, QSize
 
@@ -31,32 +31,35 @@ class ToolBar(QWidget):
         vLayout.setSpacing(0)
 
         icons = [
-                    "assets/tools/plus.png",
-                    "assets/tools/nodes.png",
-                    "assets/tools/scissors.png",
-                    "assets/tools/tick-mark.png",
-                    "assets/tools/delete.png",
-                    "assets/tools/loupe.png",
-                ]
-        # self.add_state_button = QPushButton()
-        # self.add_state_button.setIcon(QIcon("assets/tools/plus.png"))
-
-        # self.add_event_button = QPushButton()
-        # self.add_event_button.setIcon(QIcon("assets/tools/nodes.png"))
-
-        # vLayout.addWidget(self.add_state_button)
-        # vLayout.addWidget(self.add_event_button)
+            "assets/tools/plus.png",
+            "assets/tools/nodes.png",
+            "assets/tools/scissors.png",
+            "assets/tools/tick-mark.png",
+            "assets/tools/delete.png",
+            "assets/tools/loupe.png",
+        ]
 
         self.tools_list = QListWidget()
         self.tools_list_layout = QVBoxLayout()
         self.tools_list_layout.setContentsMargins(0, 0, 0, 0)
-        self.tools_list_layout.setSpacing(0)
+        self.tools_list.setSpacing(5)
+        self.tools_list.setFixedWidth(40)
         self.tools_list.setLayout(self.tools_list_layout)
+        self.tools_list.setFlow(QListWidget.Flow.TopToBottom)
+        self.tools_list.setViewMode(QListWidget.ViewMode.IconMode)
+        self.tools_list.setIconSize(QSize(20, 20))
+        self.tools_list.setMovement(QListWidget.Movement.Static)
 
-        self.tools_list.addItems(icons)
+        # self.tools_list.addItems(icons)
+        for i, icon in enumerate(icons):
+            item = QListWidgetItem()
+            item.setIcon(QIcon(icon))
+            self.tools_list.addItem(item)
+            if i == 0:
+                self.tools_list.setCurrentItem(item)
 
         vLayout.addWidget(self.tools_list, 0)
 
-        self.setStyleSheet("background-color: #ffffff; margin: 0;")
+        self.setStyleSheet("background-color: #ffffff; padding: 5px 0;")
         vLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.setLayout(vLayout)
